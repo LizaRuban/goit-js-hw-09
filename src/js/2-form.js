2914d74ad46bb4e397fb5678deda089afac323b6
Liza Ruban <yelyzaveta.ruban2023@gmail.com>
2026-04-22 22:35:46 +0300

    Add tasks

---
+++ src/js/2-form.js
@@ -0,0 +1,40 @@
+const STORAGE_KEY = 'feedback-form-state';
+
+let formData = {
+  email: '',
+  message: '',
+};
+
+const form = document.querySelector('.feedback-form');
+const savedData = localStorage.getItem(STORAGE_KEY);
+
+if (savedData) {
+  formData = JSON.parse(savedData);
+  form.elements.email.value = formData.email || '';
+  form.elements.message.value = formData.message || '';
+}
+
+form.addEventListener('input', event => {
+  const { name, value } = event.target;
+  if (name === 'email' || name === 'message') {
+    formData[name] = value.trim();
+    localStorage.setItem(STORAGE_KEY, JSON.stringify(formData));
+  }
+});
+
+form.addEventListener('submit', event => {
+  event.preventDefault();
+
+  const { email, message } = formData;
+
+  if (!email || !message) {
+    alert('Fill please all fields');
+    return;
+  }
+
+  console.log(formData);
+
+  form.reset();
+  formData = { email: '', message: '' };
+  localStorage.removeItem(STORAGE_KEY);
+});
